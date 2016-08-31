e6ac4c5793176d29e1f6f2dd780d7075f7865fb3
Edgar Ribeiro <edgar.marques.ribeiro@gmail.com>
2016-08-31 10:43:09 +0100

    use redux-promise instead of redux-thunk

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,14 +1,13 @@
 import {createStore, compose, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import promiseMiddleware from 'redux-promise';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const middewares = [
     // Add other middleware on this line...
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-    thunkMiddleware,
+    // promise middleware
+    promiseMiddleware
   ];
 
   return createStore(rootReducer, initialState, compose(
