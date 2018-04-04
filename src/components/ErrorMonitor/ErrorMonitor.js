603f95becc111c816310542b774d6769a70aadcb
mstrk <mstrk512@gmail.com>
2018-04-04 21:03:59 +0100

    feat(ErrorMonitor): added error reporting to mock api

--- src/components/ErrorMonitor/ErrorMonitor.js
+++ src/components/ErrorMonitor/ErrorMonitor.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import { node } from 'prop-types'
+import request from 'util/request'
+import { API_URL } from 'constants/endpoints'
+
+const __DEV__ = process.env.NODE_ENV !== 'production'
 
 class ErrorMonitor extends Component {
   state = {
@@ -9,19 +13,37 @@ class ErrorMonitor extends Component {
   }
 
   componentDidCatch (error, info) {
-    // Display fallback UI
-    this.setState({ hasError: true, error, errorStack: info.componentStack })
+    if (__DEV__) {
+      // Display fallback UI
+      this.setState({ hasError: true, error, errorStack: info.componentStack })
+    } else {
+      // Log the error to an error reporting service
+      // we don't care if it was successful or not
+      const headers = { 'Content-Type': 'application/json' }
+      const body = JSON.stringify({ error: error.toString(), info })
+      request(`${API_URL}/errors`, { method: 'POST', headers, body })
+    }
+  }
 
-    // Log the error to an error reporting service
-    // logErrorToMyService(error, info)
+  refreshWindow = () => {
+    location.reload()
   }
 
   renderError () {
     const { error, errorStack } = this.state
     const stack = errorStack.split('\n').map((line, indx) => <div key={indx}>{line}</div>)
+
+    if (__DEV__) {
+      return [
+        <div key='error'>{error.toString()}</div>,
+        ...stack,
+        <button key='refresh' onMouseUp={this.refreshWindow}>RELOAD</button>
+      ]
+    }
+
     return [
-      <div key='error'>{error.toString()}</div>,
-      ...stack
+      <div key='message'>Something went wrong! We are trying to fix it.</div>,
+      <button key='refresh' onMouseUp={this.refreshWindow}>RELOAD</button>
     ]
   }
 
