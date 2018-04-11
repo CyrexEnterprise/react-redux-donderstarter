65fad118f409b6a961c80b5ffee26310d23ae03a
mstrk <mstrk512@gmail.com>
2018-04-11 23:38:57 +0100

    build(generate): missing component name

--- internals/generate/componentStateless/templates/tests.js
+++ internals/generate/componentStateless/templates/tests.js
@@ -1,11 +1,11 @@
 module.exports = function (name) {
   return `import React from 'react'
 import { shallow } from 'enzyme'
-import Foo from './Foo'
+import ${name} from './${name}'
 
-describe('<Foo />', () => {
+describe('<${name} />', () => {
   const spy = jest.fn()
-  const wrapper = shallow(<Foo counter={0} onButtonClick={spy} />)
+  const wrapper = shallow(<${name} counter={0} onButtonClick={spy} />)
 
   it('should call onButtonClick', () => {
     expect(spy).not.toHaveBeenCalled()
