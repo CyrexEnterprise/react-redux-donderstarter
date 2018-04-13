04c4db8f69a19a8061ceb955c6f5b1b677af7512
mstrk <mstrk512@gmail.com>
2018-04-13 12:10:31 +0100

    docs(Navigation): added missing decorator in stories

--- src/components/Navigation/stories.js
+++ src/components/Navigation/stories.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { Provider } from 'react-redux'
+import store from 'store'
+import { translationMessages, formats } from 'util/i18n'
+import LanguageProvider from 'containers/LanguageProvider'
 import Navigation from './Navigation'
 
 storiesOf('Navigation', module)
+  .addDecorator(render => (
+    <Provider store={store}>
+      <LanguageProvider messages={translationMessages} formats={formats}>
+        {render()}
+      </LanguageProvider>
+    </Provider>
+  ))
   .addWithInfo('default', () => (
     <Navigation title='DonderStarter' />
   ))
